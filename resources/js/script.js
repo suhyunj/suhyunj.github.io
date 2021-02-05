c05f0cf64d33b3646a9dcd652284eb77fed156b2
Su Hyun <59631402+suhyunj@users.noreply.github.com>
2021-02-04 21:44:42 -0800

    update

--- resources/js/script.js
+++ resources/js/script.js
@@ -63,21 +63,4 @@ $(document).ready(function(){
 $('#example').zoom(); // add zoom
 $('#example').trigger('zoom.destroy'); // remove zoom
 
-//MOUSE CURSOR
-$(function(){
-	 $('.row').mousefollower({
-         follwerSelector:".cursor"
-         
-     });
-});
 
-var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36251023-1']);
-  _gaq.push(['_setDomainName', 'jqueryscript.net']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
